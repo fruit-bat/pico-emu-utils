5c2215a24da36874ee34fb6f8fa4eb700ca82dbe
Phil <pmscull@gmail.com>
2022-07-09 22:46:56 +0100

    PS/2 keyboard support

--- src/ps2kbd.cpp
+++ src/ps2kbd.cpp
@@ -14,6 +14,7 @@
 
 #define HID_KEYBOARD_REPORT_MAX_KEYS 6
 
+// PS/2 set 2 to HID key conversion
 static uint8_t ps2kbd_page_0[] {
   /* 00 (  0) */ HID_KEY_NONE,
   /* 01 (  1) */ HID_KEY_F9,
@@ -23,23 +24,23 @@ static uint8_t ps2kbd_page_0[] {
   /* 05 (  5) */ HID_KEY_F1,
   /* 06 (  6) */ HID_KEY_F2,
   /* 07 (  7) */ HID_KEY_F12,
-  /* 08 (  8) */ 0x00,
+  /* 08 (  8) */ HID_KEY_F13,
   /* 09 (  9) */ HID_KEY_F10,
   /* 0A ( 10) */ HID_KEY_F8,
   /* 0B ( 11) */ HID_KEY_F6,
   /* 0C ( 12) */ HID_KEY_F4,
   /* 0D ( 13) */ HID_KEY_TAB,
   /* 0E ( 14) */ HID_KEY_GRAVE,
-  /* 0F ( 15) */ 0x00,
-  /* 10 ( 16) */ 0x00,
-  /* 11 ( 17) */ 0x00,
-  /* 12 ( 18) */ 0x00,
+  /* 0F ( 15) */ HID_KEY_KEYPAD_EQUAL,
+  /* 10 ( 16) */ HID_KEY_F14,
+  /* 11 ( 17) */ HID_KEY_ALT_LEFT,
+  /* 12 ( 18) */ HID_KEY_SHIFT_LEFT,
   /* 13 ( 19) */ 0x00,
-  /* 14 ( 20) */ 0x00,
+  /* 14 ( 20) */ HID_KEY_CONTROL_LEFT,
   /* 15 ( 21) */ HID_KEY_Q,
   /* 16 ( 22) */ HID_KEY_1,
   /* 17 ( 23) */ 0x00,
-  /* 18 ( 24) */ 0x00,
+  /* 18 ( 24) */ HID_KEY_F15,
   /* 19 ( 25) */ 0x00,
   /* 1A ( 26) */ HID_KEY_Z,
   /* 1B ( 27) */ HID_KEY_S,
@@ -47,7 +48,7 @@ static uint8_t ps2kbd_page_0[] {
   /* 1D ( 29) */ HID_KEY_W,
   /* 1E ( 30) */ HID_KEY_2,
   /* 1F ( 31) */ 0x00,
-  /* 20 ( 32) */ 0x00,
+  /* 20 ( 32) */ HID_KEY_F16,
   /* 21 ( 33) */ HID_KEY_C,
   /* 22 ( 34) */ HID_KEY_X,
   /* 23 ( 35) */ HID_KEY_D,
@@ -55,7 +56,7 @@ static uint8_t ps2kbd_page_0[] {
   /* 25 ( 37) */ HID_KEY_4,
   /* 26 ( 38) */ HID_KEY_3,
   /* 27 ( 39) */ 0x00,
-  /* 28 ( 40) */ 0x00,
+  /* 28 ( 40) */ HID_KEY_F17,
   /* 29 ( 41) */ HID_KEY_SPACE,
   /* 2A ( 42) */ HID_KEY_V,
   /* 2B ( 43) */ HID_KEY_F,
@@ -63,7 +64,7 @@ static uint8_t ps2kbd_page_0[] {
   /* 2D ( 45) */ HID_KEY_R,
   /* 2E ( 46) */ HID_KEY_5,
   /* 2F ( 47) */ 0x00,
-  /* 30 ( 48) */ 0x00,
+  /* 30 ( 48) */ HID_KEY_F18,
   /* 31 ( 49) */ HID_KEY_N,
   /* 32 ( 50) */ HID_KEY_B,
   /* 33 ( 51) */ HID_KEY_H,
@@ -71,7 +72,7 @@ static uint8_t ps2kbd_page_0[] {
   /* 35 ( 53) */ HID_KEY_Y,
   /* 36 ( 54) */ HID_KEY_6,
   /* 37 ( 55) */ 0x00,
-  /* 38 ( 56) */ 0x00,
+  /* 38 ( 56) */ HID_KEY_F19,
   /* 39 ( 57) */ 0x00,
   /* 3A ( 58) */ HID_KEY_M,
   /* 3B ( 59) */ HID_KEY_J,
@@ -79,7 +80,7 @@ static uint8_t ps2kbd_page_0[] {
   /* 3D ( 61) */ HID_KEY_7,
   /* 3E ( 62) */ HID_KEY_8,
   /* 3F ( 63) */ 0x00,
-  /* 40 ( 64) */ 0x00,
+  /* 40 ( 64) */ HID_KEY_F20,
   /* 41 ( 65) */ HID_KEY_COMMA,
   /* 42 ( 66) */ HID_KEY_K,
   /* 43 ( 67) */ HID_KEY_I,
@@ -87,7 +88,7 @@ static uint8_t ps2kbd_page_0[] {
   /* 45 ( 69) */ HID_KEY_0,
   /* 46 ( 70) */ HID_KEY_9,
   /* 47 ( 71) */ 0x00,
-  /* 48 ( 72) */ 0x00,
+  /* 48 ( 72) */ HID_KEY_F21,
   /* 49 ( 73) */ HID_KEY_PERIOD,
   /* 4A ( 74) */ HID_KEY_SLASH,
   /* 4B ( 75) */ HID_KEY_L,
@@ -95,22 +96,22 @@ static uint8_t ps2kbd_page_0[] {
   /* 4D ( 77) */ HID_KEY_P,
   /* 4E ( 78) */ HID_KEY_MINUS,
   /* 4F ( 79) */ 0x00,
-  /* 50 ( 80) */ 0x00,
+  /* 50 ( 80) */ HID_KEY_F22,
   /* 51 ( 81) */ 0x00,
   /* 52 ( 82) */ HID_KEY_APOSTROPHE,
   /* 53 ( 83) */ 0x00,
   /* 54 ( 84) */ HID_KEY_BRACKET_LEFT,
   /* 55 ( 85) */ HID_KEY_EQUAL,
   /* 56 ( 86) */ 0x00,
-  /* 57 ( 87) */ 0x00,
+  /* 57 ( 87) */ HID_KEY_F23,
   /* 58 ( 88) */ HID_KEY_CAPS_LOCK,
-  /* 59 ( 89) */ 0x00,
+  /* 59 ( 89) */ HID_KEY_SHIFT_RIGHT,
   /* 5A ( 90) */ HID_KEY_ENTER, // RETURN ??
   /* 5B ( 91) */ HID_KEY_BRACKET_RIGHT,
   /* 5C ( 92) */ 0x00,
   /* 5D ( 93) */ HID_KEY_BACKSLASH,
   /* 5E ( 94) */ 0x00,
-  /* 5F ( 95) */ 0x00,
+  /* 5F ( 95) */ HID_KEY_F24,
   /* 60 ( 96) */ 0x00,
   /* 61 ( 97) */ 0x00,
   /* 62 ( 98) */ 0x00,
@@ -120,81 +121,33 @@ static uint8_t ps2kbd_page_0[] {
   /* 66 (102) */ HID_KEY_BACKSPACE,
   /* 67 (103) */ 0x00,
   /* 68 (104) */ 0x00,
-  /* 69 (105) */ 0x00,
+  /* 69 (105) */ HID_KEY_KEYPAD_1,
   /* 6A (106) */ 0x00,
-  /* 6B (107) */ 0x00,
-  /* 6C (108) */ 0x00,
+  /* 6B (107) */ HID_KEY_KEYPAD_4,
+  /* 6C (108) */ HID_KEY_KEYPAD_7,
   /* 6D (109) */ 0x00,
   /* 6E (110) */ 0x00,
   /* 6F (111) */ 0x00,
-  /* 70 (112) */ 0x00,
-  /* 71 (113) */ 0x00,
-  /* 72 (114) */ 0x00,
-  /* 73 (115) */ 0x00,
-  /* 74 (116) */ 0x00,
-  /* 75 (117) */ 0x00,
+  /* 70 (112) */ HID_KEY_KEYPAD_0,
+  /* 71 (113) */ HID_KEY_KEYPAD_DECIMAL,
+  /* 72 (114) */ HID_KEY_KEYPAD_2,
+  /* 73 (115) */ HID_KEY_KEYPAD_5,
+  /* 74 (116) */ HID_KEY_KEYPAD_6,
+  /* 75 (117) */ HID_KEY_KEYPAD_8,
   /* 76 (118) */ HID_KEY_ESCAPE,
-  /* 77 (119) */ 0x00,
+  /* 77 (119) */ HID_KEY_NUM_LOCK,
   /* 78 (120) */ HID_KEY_F11,
-  /* 79 (121) */ 0x00,
-  /* 7A (122) */ 0x00,
-  /* 7B (123) */ 0x00,
-  /* 7C (124) */ 0x00,
-  /* 7D (125) */ 0x00,
-  /* 7E (126) */ 0x00,
+  /* 79 (121) */ HID_KEY_KEYPAD_ADD,
+  /* 7A (122) */ HID_KEY_KEYPAD_3,
+  /* 7B (123) */ HID_KEY_KEYPAD_SUBTRACT,
+  /* 7C (124) */ HID_KEY_KEYPAD_MULTIPLY,
+  /* 7D (125) */ HID_KEY_KEYPAD_9,
+  /* 7E (126) */ HID_KEY_SCROLL_LOCK,
   /* 7F (127) */ 0x00,
   /* 80 (128) */ 0x00,
   /* 81 (129) */ 0x00,
   /* 82 (130) */ 0x00,
-  /* 83 (131) */ HID_KEY_F7,
-  /* 84 (132) */ 0x00,
-  /* 85 (133) */ 0x00,
-  /* 86 (134) */ 0x00,
-  /* 87 (135) */ 0x00,
-  /* 88 (136) */ 0x00,
-  /* 89 (137) */ 0x00,
-  /* 8A (138) */ 0x00,
-  /* 8B (139) */ 0x00,
-  /* 8C (140) */ 0x00,
-  /* 8D (141) */ 0x00,
-  /* 8E (142) */ 0x00,
-  /* 8F (143) */ 0x00,
-  /* 90 (144) */ 0x00,
-  /* 91 (145) */ 0x00,
-  /* 92 (146) */ 0x00,
-  /* 93 (147) */ 0x00,
-  /* 94 (148) */ 0x00,
-  /* 95 (149) */ 0x00,
-  /* 96 (150) */ 0x00,
-  /* 97 (151) */ 0x00,
-  /* 98 (152) */ 0x00,
-  /* 99 (153) */ 0x00,
-  /* 9A (154) */ 0x00,
-  /* 9B (155) */ 0x00,
-  /* 9C (156) */ 0x00,
-  /* 9D (157) */ 0x00,
-  /* 9E (158) */ 0x00,
-  /* 9F (159) */ 0x00,
-  /* A0 (160) */ 0x00,
-  /* A1 (161) */ 0x00,
-  /* A2 (162) */ 0x00,
-  /* A3 (163) */ 0x00,
-  /* A4 (164) */ 0x00,
-  /* A5 (165) */ 0x00,
-  /* A6 (166) */ 0x00,
-  /* A7 (167) */ 0x00,
-  /* A8 (168) */ 0x00,
-  /* A9 (169) */ 0x00,
-  /* AA (170) */ 0x00,
-  /* AB (171) */ 0x00,
-  /* AC (172) */ 0x00,
-  /* AD (173) */ 0x00,
-  /* AE (174) */ 0x00,
-  /* AF (175) */ 0x00,
-  /* B0 (176) */ 0x00,
-  /* B1 (177) */ 0x00,
-  /* B2 (178) */ 0x00,
-  /* B3 (179) */ 0x00
+  /* 83 (131) */ HID_KEY_F7
 };
 
 Ps2Kbd::Ps2Kbd(PIO pio, uint base_gpio) :
@@ -212,11 +165,18 @@ uint8_t Ps2Kbd::hidCodePage0(uint8_t ps2code) {
   return ps2code < sizeof(ps2kbd_page_0) ? ps2kbd_page_0[ps2code] : HID_KEY_NONE;
 }
 
+// PS/2 set 2 to HID key conversion
 uint8_t Ps2Kbd::hidCodePage1(uint8_t ps2code) {
   switch(ps2code) {
+// TODO these belong to a different HID usage page
 //  case 0x37: return HID_KEY_POWER;
 //  case 0x3f: return HID_KEY_SLEEP;
 //  case 0x5e: return HID_KEY_WAKE;
+  case 0x11: return HID_KEY_ALT_RIGHT;
+  case 0x1f: return HID_KEY_GUI_LEFT;
+  case 0x14: return HID_KEY_CONTROL_RIGHT;
+  case 0x27: return HID_KEY_GUI_RIGHT;
+  case 0x5a: return HID_KEY_KEYPAD_ENTER;
   case 0x7c: return HID_KEY_PRINT_SCREEN;
   case 0x70: return HID_KEY_INSERT;
   case 0x6c: return HID_KEY_HOME;
@@ -228,7 +188,7 @@ uint8_t Ps2Kbd::hidCodePage1(uint8_t ps2code) {
   case 0x6b: return HID_KEY_ARROW_LEFT;
   case 0x72: return HID_KEY_ARROW_DOWN;
   case 0x75: return HID_KEY_ARROW_UP;
-  case 0x5a: return HID_KEY_KEYPAD_DIVIDE;
+  case 0x4a: return HID_KEY_KEYPAD_DIVIDE;
 
   default: 
     return HID_KEY_NONE;
