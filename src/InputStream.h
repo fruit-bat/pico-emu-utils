ed0a1f20d90197e2f79324945121273280e41bce
zappa4me <122390465+zappa4me@users.noreply.github.com>
2024-05-20 13:22:36 +0200

    fix linker issues regarding missing vtable

--- src/InputStream.h
+++ src/InputStream.h
@@ -1,16 +1,17 @@
 #pragma once
+#include <cstdint>
 #include <stdio.h>
 
 class InputStream {
-  
+
 public:
   virtual ~InputStream() { close(); }
-  virtual int32_t readByte(); // read a single byte, -1 for eof, -ve for error
+  virtual int32_t readByte() = 0; // read a single byte, -1 for eof, -ve for error
   virtual int32_t readWord(); // read 2 bytes LSB first, -1 for eof, -ve for error
   virtual int32_t read(uint8_t* buffer, const uint32_t length); // returns actual length read, -1 for eof, -ve for error
   virtual void close() {}
-  virtual bool closed();
-  virtual bool end();
+  virtual bool closed() = 0;
+  virtual bool end() = 0;
   virtual int32_t seek(const uint32_t pos) { return -2; }
   virtual int32_t rseek(const int32_t rpos) { return -2; }
   virtual uint32_t pos() { return 0; }
