5d63d9a993caa02f1e36c314a8dae2a4f7d931ea
Phil <pmscull@gmail.com>
2025-10-05 10:26:09 +0100

    RP2350PiZero

--- src/SdCardFatFsSpi.cpp
+++ src/SdCardFatFsSpi.cpp
@@ -4,6 +4,8 @@
 #include "ff.h"
 #include "ff_util.h"
 #include "pico/stdlib.h"
+#include "hardware/gpio.h"
+#include "hardware/pio.h"
 
 #ifdef DEBUG_FAT_SPI
 #define DBG_PRINTF(...) printf(__VA_ARGS__)
@@ -20,6 +22,14 @@ SdCardFatFsSpi::SdCardFatFsSpi(int unit) :
 bool SdCardFatFsSpi::mount() {
   if (mounted()) return true;
   DBG_PRINTF("Mounting SD card %d... ", _unit);
+
+  // This is needed for using high GPIO for the SDCARD PIO
+#ifdef SDCARD_PIO
+#if SDCARD_PIN_SPI0_CS > 31 || SDCARD_PIN_SPI0_SCK > 31 || SDCARD_PIN_SPI0_MOSI > 31 || SDCARD_PIN_SPI0_MISO > 31
+  pio_set_gpio_base(SDCARD_PIO, 16);
+#endif
+#endif
+
   FRESULT fr = f_mount(&fs, "", 1);
   if (FR_OK != fr) {
     DBG_PRINTF(" error: %s (%d)\n", FRESULT_str(fr), fr);
